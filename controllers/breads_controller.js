e1d4d1ffc866ef2be92018102073875b997f7c81
Brian Makori <bemakori@gmail.com>
2022-02-12 13:32:37 -0500

    added views

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,7 +4,12 @@ const Bread = require('../models/bread.js')
 
 // Index
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('Index',
+    {
+      breads: Bread
+    }
+  )
+// res.send(Bread)
 })
 
 module.exports = breads
